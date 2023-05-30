ac7f7a83785d40b15e7975011f917a45dcac5372
Zac <121649297+zac-march@users.noreply.github.com>
2023-05-30 19:49:54 +1000

    Add logic to detect if char is clicked

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import waldoImage from "./resources/whereiswaldo.jpg";
 import debounce from "./utils/debounce";
-import SelectedArea from "./components/AreaElement";
+import SelectedArea from "./components/SelectedArea";
+
+const CHARS = {
+  WIZARD: { x: 0.6142292490118577, y: 0.8609355246523388 },
+  ODLAW: {
+    x: 0.6015810276679842,
+    y: 0.640960809102402,
+  },
+  WALDO: {
+    x: 0.27735368956743,
+    y: 0.32926829268292684,
+  },
+};
+
+const SELECT_SIZE = 25;
 
 function App() {
   const [areaPos, setAreaPos] = useState();
-  const [areaElement, setAreaElement] = useState();
+  const [selectedArea, setSelectedArea] = useState();
+  const [selectedChar, setSelectedChar] = useState();
 
   function handleImageClick(e) {
     const image = document.querySelector("#waldo");
@@ -14,16 +29,46 @@ function App() {
       x: e.pageX / image.offsetWidth,
       y: e.pageY / image.offsetHeight,
     });
+
+    setSelectedChar(
+      getClickedChar(
+        { x: e.pageX, y: e.pageY },
+        image.offsetWidth,
+        image.offsetHeight
+      )
+    );
   }
 
   function updateAreaElement() {
-    const image = document.querySelector("#waldo");
     if (areaPos === undefined) return;
-    const pos = {
-      x: image.offsetWidth * areaPos.x,
-      y: image.offsetHeight * areaPos.y,
+    const image = document.querySelector("#waldo");
+    const imgWidth = image.offsetWidth;
+    const imgHeight = image.offsetHeight;
+    const imgPos = {
+      x: imgWidth * areaPos.x,
+      y: imgHeight * areaPos.y,
     };
-    setAreaElement(<SelectedArea pos={pos} />);
+    setSelectedArea(
+      <SelectedArea pos={imgPos} selectSize={SELECT_SIZE} chars={CHARS} />
+    );
+  }
+
+  function getClickedChar(imgPos, imgWidth, imgHeight) {
+    for (const char in CHARS) {
+      const cPos = {
+        x: CHARS[char].x * imgWidth,
+        y: CHARS[char].y * imgHeight,
+      };
+      if (
+        imgPos.x >= cPos.x - SELECT_SIZE &&
+        imgPos.x <= cPos.x + SELECT_SIZE &&
+        imgPos.y >= cPos.y - SELECT_SIZE &&
+        imgPos.y <= cPos.y + SELECT_SIZE
+      ) {
+        return char;
+      }
+    }
+    return null;
   }
 
   useEffect(() => {
@@ -40,10 +85,14 @@ function App() {
     updateAreaElement();
   }, [areaPos]);
 
+  useEffect(() => {
+    console.log(selectedChar);
+  }, [selectedChar]);
+
   return (
     <div className="App">
       <div className="game">
-        {areaElement && areaElement}
+        {selectedArea && selectedArea}
         <img
           id="waldo"
           alt="Where is waldo"
